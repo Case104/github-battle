a3af2bb5e65df86de1aec16388ce71690471ea0a
Case104 <jpcase104@gmail.com>
2016-07-03 14:01:02 -0400

    ConfirmBattleContainer added with route

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,30 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+
+	getInitialState: function(){
+		return {
+			isLoading: true,
+			playerInfo: []
+		}
+	},
+
+	componentDidMount: function(){
+		console.log(this);
+	},
+
+	render: function(){
+		return (
+			<ConfirmBattle
+				isLoading={this.state.isLoading}
+				playerInfo={this.state.playerInfo}/>
+		)
+	}
+})
+
+module.exports = ConfirmBattleContainer;
